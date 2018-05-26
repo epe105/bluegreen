24df74b3020f366f05d24e2438b18d9a6b5a9e84
Manny Evangelista <epe105@users.noreply.github.com>
2018-05-26 16:58:55 -0400

    added bluegreen-green app setup

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -23,3 +23,16 @@ oc policy add-role-to-user edit system:serviceaccount:app-dev:jenkins -n app-pro
 # Give the other related projects the role to pull images from pipeline-app
 oc policy add-role-to-group system:image-puller system:serviceaccounts:app-qa -n app-dev
 oc policy add-role-to-group system:image-puller system:serviceaccounts:app-prod -n app-qa
+
+# Create BlueGreen Green App
+oc project app-prod
+oc new-app https://github.com/epe105/bluegreen#green --name=bluegreen-green
+oc expose service bluegreen-green
+
+# Delete bluegreen app
+oc project app-dev
+oc delete service bluegreen
+oc delete deploymentconfigs bluegreen
+oc delete route bluegreen
+oc delete imagestreams bluegreen
+oc delete bc bluegreen -n app-dev
